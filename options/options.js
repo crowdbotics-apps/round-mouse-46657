ddb81f08e38f7c16bf526b00f4cbe4a3995f7f47
Crowdbotics <team@crowdbotics.com>
2024-02-27 01:42:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-mouse-46657",
+  url: "https://round-mouse-46657.botics.co",
+  api: "https://round-mouse-46657.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
